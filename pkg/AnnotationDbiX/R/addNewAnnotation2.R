ed635e01b0f95dc0c1120bdf838c97fe4c78a8f3
nauer (Norbert Auer) <norbert@derauer.net>
2010-02-10 15:35:52 +0000

    addNewAnnotationFromDb1 added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/annotationdbix@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/AnnotationDbiX/R/addNewAnnotation2.R
+++ pkg/AnnotationDbiX/R/addNewAnnotation2.R
@@ -5,10 +5,19 @@
 ## Load Library
 #require("RSQLite")
 
-setGeneric("addNewAnnotation2", signature = c("x","data","newTableName","data.colNames","mapTableName"),function(x,data,newTableName,data.colNames,mapTableName,dbSrc=NULL) standardGeneric("addNewAnnotation2"))
+# x					dbConn() - Objekt oder Pfad
+# data				Liste mit neuen AnnotationDaten 1.Spalte Ids die im Package bereits vorhanden sind; 2 Spalte neue Ids
+# newTableName		Neuer Tablename
+# data.colNames		Spalten für die neue Tabelle ohne _id
+# mapTableName		Name der Table auf die gematched wird
+# dbSrc				.db1 Datenbank Pfad oder dbConn() Objekt
+
+setGeneric("addNewAnnotation2", signature = c("x","data","newTableName","data.colNames","mapTableName"),function(x,data,newTableName,data.colNames,mapTableName) standardGeneric("addNewAnnotation2"))
+
+setGeneric("addNewAnnotationFromDb1", signature = c("x","data","mapTableName","mapDb1TableName","dbSrc"),function(x,data,mapTableName,mapDb1TableName,dbSrc) standardGeneric("addNewAnnotationFromDb1"))
 
 ## FilePath
-setMethod("addNewAnnotation2", signature("character","data.frame","character","character","character"), function(x,data,newTableName,data.colNames,mapTableName,dbSrc) 
+setMethod("addNewAnnotation2", signature("character","data.frame","character","character","character"), function(x,data,newTableName,data.colNames,mapTableName) 
 {
 	## Check Parameters
 	if(!file.exists(x))
@@ -21,11 +30,11 @@ setMethod("addNewAnnotation2", signature("character","data.frame","character","c
 	con <- dbConnect(drv, dbname = x)
 	on.exit(dbDisconnect(con))
 	
-	addNewAnnotation2(con,data,newTableName,data.colNames,mapTableName,dbSrc)
+	addNewAnnotation2(con,data,newTableName,data.colNames,mapTableName)
 })
 
 ## SQLite-Connection
-setMethod("addNewAnnotation2", signature("SQLiteConnection","data.frame","character","character","character"), function(x,data,newTableName,data.colNames,mapTableName,dbSrc) 
+setMethod("addNewAnnotation2", signature("SQLiteConnection","data.frame","character","character","character"), function(x,data,newTableName,data.colNames,mapTableName) 
 {	
 	con <- x
 	
@@ -43,78 +52,111 @@ setMethod("addNewAnnotation2", signature("SQLiteConnection","data.frame","charac
 	meta <- dbGetQuery(con,sql)
 	main_table <- meta[meta$key == 'main_table','value']
 	
-	if(is.null(dbSrc))
-	{
-		if(!(mapTableName %in% id_tables[[1]]))
-		stop("There is no table named ",mapTableName)
+	
+	if(!(mapTableName %in% id_tables[[1]]))
+	stop("There is no table named ",mapTableName)
 
-		colnames(data) <- c(as.character(id_tables[id_tables$tablename == mapTableName,'mainCol'][1]),data.colNames)
+	colnames(data) <- c(as.character(id_tables[id_tables$tablename == mapTableName,'mainCol'][1]),data.colNames)
 	
-		## Add helper table	
-		cat("Add helper table ",newTableName,"_temp\n",sep="")
-		dbWriteTable(conn=con,name=paste(newTableName,"_temp",sep=""),value=unique(data[colnames(data)]),row.names=FALSE,overwrite=TRUE)	
+	## Add helper table	
+	cat("Add helper table ",newTableName,"_temp\n",sep="")
+	dbWriteTable(conn=con,name=paste(newTableName,"_temp",sep=""),value=unique(data[colnames(data)]),row.names=FALSE,overwrite=TRUE)	
 	
-		## Create new table
-		cat("Create new table",newTableName,"\n")
-		if(length(data.colNames[-1]) != 0)
-			dyn <- paste(",",paste(data.colNames[-1],"TEXT",collapse=","))
-		else
-			dyn <- ""
-		
-		sql <- paste("CREATE TABLE",newTableName,"(_id INTEGER REFERENCES ",main_table,"(_id) NOT NULL,",colnames(data)[2]," TEXT NOT NULL",dyn,")")
-		dbGetQuery(con,sql)
-	
-		## Fill new table
-		cat("Fill new table",newTableName,"\n")
-		dyn <- paste(data.colNames,collapse=",")
-		sql <- paste("INSERT INTO ",newTableName," SELECT _id,",dyn," FROM ",newTableName,"_temp t,",mapTableName," p WHERE p.",colnames(data)[1]," = t.",colnames(data)[1]," AND p._id",sep="")
-	
-		dbGetQuery(con,sql)
-	
-		## Update table_master_meta
-		cat("Update table_master_meta\n")
-		sql <- paste("INSERT INTO table_master_meta VALUES('",newTableName,"','",paste(data.colNames,collapse=";"),"')",sep="")
-		dbGetQuery(con,sql)
-	}
+	## Create new table
+	cat("Create new table",newTableName,"\n")
+	if(length(data.colNames[-1]) != 0)
+		dyn <- paste(",",paste(data.colNames[-1],"TEXT",collapse=","))
 	else
-	{
-		if(file.exists(dbSrc))
-		{
-			## Attach Database
-			cat('Attach Database',dbSrc,'as db1\n')
-			sql <- paste("ATTACH '",dbSrc,"' AS db1",sep="")
-			dbGetQuery(con,sql)
+		dyn <- ""
 		
-			## Get colinfo
-			sql <- paste("PRAGMA table_info(",data.colNames[1],")",sep="")
-			print(sql)
-			colinfo <- dbGetQuery(con,sql)
-			toDrop <- colinfo[-1,'name']
-			print(toDrop)
-			
-			if(!(mapTableName %in% id_tables[[1]]))
-			stop("There is no table named ",mapTableName)
-			
-			## Add helper table	
-			cat("Add helper table ",newTableName,"_temp\n",sep="")
-			dbWriteTable(conn=con,name=paste(newTableName,"_temp",sep=""),value=unique(data[colnames(data)]),row.names=FALSE,overwrite=TRUE)	
+	sql <- paste("CREATE TABLE",newTableName,"(_id INTEGER REFERENCES ",main_table,"(_id) NOT NULL,",colnames(data)[2]," TEXT NOT NULL",dyn,")")
+	dbGetQuery(con,sql)
 	
-			## Create new table
-			cat("Create new table",newTableName,"\n")
-			if(length(data.colNames[-1]) != 0)
-				dyn <- paste(",",paste(data.colNames[-1],"TEXT",collapse=","))
-			else
-				dyn <- ""
-		
-			sql <- paste("CREATE TABLE",newTableName,"(_id INTEGER REFERENCES ",main_table,"(_id) NOT NULL,",colnames(data)[2]," TEXT NOT NULL",dyn,")")
-			dbGetQuery(con,sql)
-		}
-		else
-			stop("'dbScr' does not exist!")
-	}
+	## Fill new table
+	cat("Fill new table",newTableName,"\n")
+	dyn <- paste(data.colNames,collapse=",")
+	sql <- paste("INSERT INTO ",newTableName," SELECT _id,",dyn," FROM ",newTableName,"_temp t,",mapTableName," p WHERE p.",colnames(data)[1]," = t.",colnames(data)[1]," AND p._id",sep="")
+	
+	dbGetQuery(con,sql)
+	
+	## Update table_master_meta
+	cat("Update table_master_meta\n")
+	sql <- paste("INSERT INTO table_master_meta VALUES('",newTableName,"','",paste(data.colNames,collapse=";"),"')",sep="")
+	dbGetQuery(con,sql)
+	
+	
 	
 	## Remove helper table
 	cat("Remove helper table\n")
 	sql <- paste("DROP TABLE ",newTableName,"_temp",sep="")
 	dbGetQuery(con,sql)
+})
+
+## FilePath 
+setMethod("addNewAnnotationFromDb1", signature("character","data.frame","character","character","ANY"), function(x,data,mapTableName,mapDb1TableName,dbSrc) 
+{
+	## Check Parameters
+	if(!file.exists(x))
+		stop("Database do not exist!")
+	
+	## Load SQLite Driver
+	drv <- dbDriver("SQLite")
+	
+	## Generate Connection Object	
+	con <- dbConnect(drv, dbname = x)
+	on.exit(dbDisconnect(con))
+	
+	addNewAnnotationFromDb1(con,data,mapTableName,mapDb1TableName,dbSrc)
+})
+
+## SQLite-Connection
+setMethod("addNewAnnotationFromDb1", signature("SQLiteConnection","data.frame","character","character","character"), function(x,data,mapTableName,mapDb1TableName,dbSrc) 
+{	
+	con <- x
+	
+	if(!file.exists(dbSrc))
+		stop("dbScr '",dbSrc,"' does not exist!")	
+	
+	## Load SQLite Driver
+	drv <- dbDriver("SQLite")
+	
+	## Generate Connection Object	
+	con2 <- dbConnect(drv, dbname = x)
+	on.exit(dbDisconnect(con2))
+	
+	addNewAnnotationFromDb1(con,data,mapTableName,mapDb1TableName,con2)
+})
+
+setMethod("addNewAnnotationFromDb1", signature("SQLiteConnection","data.frame","character","character","SQLiteConnection"), function(x,data,mapTableName,mapDb1TableName,dbSrc) 
+{
+	con <- x
+	
+	## Attach Database
+	cat('Attach Database',dbSrc,'as db1\n')
+	sql <- paste("ATTACH '",dbSrc,"' AS db1",sep="")
+	dbGetQuery(con,sql)
+		
+	## Get colinfo
+	sql <- paste("PRAGMA table_info(",data.colNames[1],")",sep="")
+	print(sql)
+	colinfo <- dbGetQuery(con,sql)
+	toDrop <- colinfo[-1,'name']
+	print(toDrop)
+			
+	if(!(mapTableName %in% id_tables[[1]]))
+	stop("There is no table named ",mapTableName)
+			
+	## Add helper table	
+	cat("Add helper table ",newTableName,"_temp\n",sep="")
+	dbWriteTable(conn=con,name=paste(newTableName,"_temp",sep=""),value=unique(data[colnames(data)]),row.names=FALSE,overwrite=TRUE)	
+	
+	## Create new table
+	cat("Create new table",newTableName,"\n")
+	if(length(data.colNames[-1]) != 0)
+		dyn <- paste(",",paste(data.colNames[-1],"TEXT",collapse=","))
+	else
+		dyn <- ""
+		
+	sql <- paste("CREATE TABLE",newTableName,"(_id INTEGER REFERENCES ",main_table,"(_id) NOT NULL,",colnames(data)[2]," TEXT NOT NULL",dyn,")")
+	dbGetQuery(con,sql)	
 })
